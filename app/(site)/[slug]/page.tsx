7679ab07fb802ee659f038839216fb05d8a967c6
boluwatifeajayi <bolu4good@gmail.com>
2023-11-07 02:43:30 +0100

    Please

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -11,6 +11,7 @@ export default async function Page({ params }: Props) {
   return (
     <div><h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">{page?.title}</h1>
       <div className="text-lg text-gray-700 mt-10"><PortableText value={page?.content} /></div>
+      {/* https://api.vercel.com/v1/integrations/deploy/prj_1KFRvJ4IxM5iEFHaBdX4LYJsZ5LH/Z5IeSF6OpX */}
     </div>
   )
 }
---
+++ dist/static/index-9357fb28-e06001f2.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as t,DocumentPane as a}from"./index-6c71f0c1-daa2a496.js";import"./sanity-46aa66c6.js";export{t as DocumentPaneProvider,a as default};
